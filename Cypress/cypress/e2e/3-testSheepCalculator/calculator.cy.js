bc1c5f37532fab48ea9cdf91f3482c7da17ac57a
Juan Quintana <quintanajuanchy25@gmail.com>
2025-11-19 11:29:19 -0300

    feat(calculator): add TC-CALC02 for divide-by-zero validation

--- Cypress/cypress/e2e/3-testSheepCalculator/calculator.cy.js
+++ Cypress/cypress/e2e/3-testSheepCalculator/calculator.cy.js
@@ -16,11 +16,15 @@ describe("TestSheepNZ Basic Calculator — Functional Validations (Junior-Interm
     cy.get("#number1Field").should("have.value", "");
     cy.get("#number2Field").should("have.value", "");
     cy.get("#numberAnswerField").should("have.value", "");
-    cy.get("#integerSelect").should('not.be.checked')
+    cy.get("#integerSelect").should("not.be.checked");
 
     // Steps
-    cy.get("#number1Field").type(number1Field).should("have.value", number1Field.toString());
-    cy.get("#number2Field").type(number2Field).should("have.value", number2Field.toString());;
+    cy.get("#number1Field")
+      .type(number1Field)
+      .should("have.value", number1Field.toString());
+    cy.get("#number2Field")
+      .type(number2Field)
+      .should("have.value", number2Field.toString());
 
     cy.get("#selectOperationDropdown")
       .should("have.value", "0")
@@ -35,9 +39,53 @@ describe("TestSheepNZ Basic Calculator — Functional Validations (Junior-Interm
 
     // Expected Result
     cy.get("#numberAnswerField")
-        .should('be.visible')
-        .should("have.value", operationResult.toString());
+      .should("be.visible")
+      .should("have.value", operationResult.toString());
 
-    cy.get("#errorMsgField").should("not.be.visible")
+    cy.get("#errorMsgField").should("not.be.visible");
+  });
+
+  it("TC-02 - Divide by Zero", () => {
+    const number1Field = 10;
+    const number2Field = 0;
+
+    // Pre Conditions
+    cy.title().should("eq", "Basic Calculator");
+    cy.get("#calcForm").should("be.visible");
+    cy.get("#errorMsgField").should("not.be.visible");
+
+
+    // Data Test
+    cy.get("#number1Field").should("have.value", "");
+    cy.get("#number2Field").should("have.value", "");
+    cy.get("#numberAnswerField").should("have.value", "");
+    cy.get("#integerSelect").should("not.be.checked");
+
+    // Steps
+    cy.get("#number1Field")
+      .type(number1Field)
+      .should("have.value", number1Field.toString());
+    cy.get("#number2Field")
+      .type(number2Field)
+      .should("have.value", number2Field.toString());
+
+    cy.get("#selectOperationDropdown")
+      .select("3")
+      .should("have.value", "3")
+      .find("option:selected")
+      .should("have.text", "Divide");
+
+    cy.get("#calculateButton")
+      .should("have.prop", "tagName", "INPUT")
+      .should("have.value", "Calculate")
+      .should("have.prop", "type", "button")
+      .click();
+
+    // Expected Result
+    cy.get("#numberAnswerField").should("not.be.visible").should("have.value", "");
+
+    cy.get("#errorMsgField")
+      .should("be.visible")
+      .should("contain.text", "Divide by zero error!");
   });
 });
