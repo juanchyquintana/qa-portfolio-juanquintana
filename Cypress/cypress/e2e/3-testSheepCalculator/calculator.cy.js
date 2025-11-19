9b2bd107ce3bd07c5f065e53cf823deb6a8e1953
Juan Quintana <quintanajuanchy25@gmail.com>
2025-11-19 10:30:45 -0300

    practice: continue with udemy classes

---
+++ Cypress/cypress/e2e/3-testSheepCalculator/calculator.cy.js
@@ -0,0 +1,7 @@
+describe("TestSheepNZ Basic Calculator — Functional Validations (Junior-Intermediate Level)", () => {
+    beforeEach(() => {
+        cy.visit("https://testsheepnz.github.io/BasicCalculator.html")
+    })
+
+    it("TC-01 - Add Basic", () => {})
+})
